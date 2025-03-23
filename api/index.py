71142276b7fe1597a20b0af01fd376e968403cc9
Jeffin Basil <105587250+FALLEN-01@users.noreply.github.com>
2025-03-23 17:24:33 +0530

    added requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,4 +1,15 @@
-from app import app
+from flask import Flask
+import sys
+import os
+
+# Add parent directory to path so we can import from app.py
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+# Import the Flask app instance
+from app import app as flask_app
+
+# This is required for Vercel serverless functions
+app = flask_app
 
 # This is the handler Vercel will invoke
 
